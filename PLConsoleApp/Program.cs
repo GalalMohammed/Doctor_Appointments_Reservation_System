534064336b2369a2fddedd1006c0370a28ac2a20
Galal Mohammed <galalmoh2001@gmail.com>
2025-03-27 14:16:14 +0200

    Add initial project structure with MVC setup, including views, controllers, and data context

---
+++ PLConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PLConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

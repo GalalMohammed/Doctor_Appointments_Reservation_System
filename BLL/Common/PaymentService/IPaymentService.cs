fe2b60a8c568f076f3117910089c265e4dba762a
ahmed-elshamy23 <ahmedm.elshamy23@gmail.com>
2025-04-13 20:08:27 +0200

    Added Stripe Payment

---
+++ BLL/Common/PaymentService/IPaymentService.cs
@@ -0,0 +1,9 @@
+﻿using Stripe.Checkout;
+
+namespace BLLServices.Common.PaymentService
+{
+    public interface IPaymentService
+    {
+        Task<Session> Pay(int doctorId, string host);
+    }
+}
---
+++ MVC/Controllers/PaymentController.cs
@@ -0,0 +1,23 @@
+﻿using BLLServices.Common.PaymentService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers
+{
+    //[Authorize]
+    public class PaymentController : Controller
+    {
+        private readonly IPaymentService paymentService;
+        public PaymentController(IPaymentService paymentService)
+        {
+            this.paymentService = paymentService;
+        }
+        [HttpPost("checkout")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Checkout(int doctorId)
+        {
+            var session = await paymentService.Pay(doctorId, Request.Host.ToString());
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+        }
+    }
+}
